69e329d218a266cc55535e4559f14320917bd279
stacy <stacydn23@gmail.com>
2021-04-04 23:51:34 -0400

    commit tree 49a0d6f1e159b59dc883f053ed34be7e347a7f1a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/signup', to: 'users#new'
+
+  post 'users', to: 'users#create'
+
+  get 'home', to: 'users#index'
+
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
 end
